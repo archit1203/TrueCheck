991ba25515b2bacaca18e7dd9691b32d46091692
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2023-04-08 01:30:34 +0530

    Create app.py

--- app.py
+++ app.py
@@ -1,40 +1,26 @@
-from flask import Flask, request, render_template, Response
-import cv2
-import numpy as np
-import qrcode
-import pyzbar.pyzbar as pyzbar
+from flask import Flask, render_template, request, redirect, url_for
+from pyzbar.pyzbar import decode
+from PIL import Image
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
-
-def gen_frames():
-    while True:
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            decoded = pyzbar.decode(frame)
-            if decoded:
-                yield f"data: {decoded[0].data.decode('utf-8')}\n\n"
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\n'
-                   b'Content-Type: image/jpeg\n\n' + frame + b'\n')  # concat frame one by one and show result
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+def index():
+    return render_template('index.html')
 
 @app.route('/scan', methods=['POST'])
 def scan():
     file = request.files['image']
-    img = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-    decoded = pyzbar.decode(img)
-    return decoded[0].data.decode('utf-8')
+    img = Image.open(file.stream)
+    result = decode(img)
+    if result:
+        return redirect(url_for('result', code=result[0][0]))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/result/<code>')
+def result(code):
+    return render_template('result.html', code=code)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
